dfcef39868bdb09afbe4eefc8bdd7d16ffa85993
lw <me@lw.ag>
2018-04-11 00:53:54 +0200

    Remove testing code.

--- src/services/user.ts
+++ src/services/user.ts
@@ -10,16 +10,8 @@ export class UserService {
   public async login(username: string, password: string) {
     const res = await this.req.post('/user/login', { username, password });
 
-    console.log(res);
-
     if (res.statusCode === 200) {
       return;
     }
-  }
-
-  public async account() {
-    const res = await this.req.get('/account');
-
-    return res;
   }
 }
