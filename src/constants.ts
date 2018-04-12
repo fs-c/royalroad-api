0f450bef333294e3ca06e6710d1c024adaee3ed3
lw <me@lw.ag>
2018-04-12 12:25:11 +0200

    Send version with userAgent and add JSdoc.

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,28 @@
+import { readFileSync } from 'fs';
+
 const PROTOCOL_PREFIX = 'https://';
 const PROTOCOL_PREFIX_INSECURE = 'http://';
 
 const HOST_NAME = 'royalroadl.com';
+
+export function getVersion() {
+  const { version } = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+  return version;
+}
+
 /**
  * @param insecure - HTTP / HTTPS
- * @returns - Bbase address with prepended protocol.
+ * @returns - Base address with prepended protocol.
  */
 export function getBaseAddress(insecure = false) {
   return (insecure ? PROTOCOL_PREFIX_INSECURE : PROTOCOL_PREFIX) + HOST_NAME;
 }
 
+/**
+ * @returns - The user agent sent along with every request, containing name
+ * and version.
+ */
 export function getUserAgent() {
-  return `royalroad-api/x.x.x (Node.js)`; // TODO, get version.
+  return `royalroadl-api/${getVersion()}`;
 }
