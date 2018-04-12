623c13b7f078ae570d2bf8a0d936b7a04d028803
lw <me@lw.ag>
2018-04-12 04:08:41 +0200

    Implement getMyFictions.

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,4 +1,10 @@
+import * as cheerio from 'cheerio';
 import { Requester } from '../royalroad';
+
+interface MyFiction {
+  id: number;
+  title: string;
+}
 
 export class UserService {
   private readonly req: Requester;
@@ -26,5 +32,29 @@ export class UserService {
     const res = await this.req.get('/account');
 
     return res;
+  }
+
+  public async getMyFictions() {
+    const html = await this.req.get('/my/fictions');
+    const myFictions = UserParser.parseMyFictions(html);
+
+    return myFictions;
+  }
+}
+
+class UserParser {
+  public static parseMyFictions(html: string): MyFiction[] {
+    const $ = cheerio.load(html);
+
+    const fictions: MyFiction[] = [];
+
+    $('div.fiction').each((i, el) => {
+      const title = $(el).find('h4.col-sm-10').text();
+      const id = parseInt($(el).find('a').attr('href').split('/')[2], 10);
+
+      fictions.push({ id, title });
+    });
+
+    return fictions;
   }
 }
