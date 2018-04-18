650860a16f0967ba37f659d944776c16bb662077
lw <me@lw.ag>
2018-04-18 15:50:05 +0200

    Implement getRevies. (closes #11)

--- src/services/fiction.ts
+++ src/services/fiction.ts
@@ -1,8 +1,8 @@
 import date = require( 'date.js');
 import * as cheerio from 'cheerio';
+import { URLSearchParams } from 'url';
 import { Requester } from '../royalroad';
 import { RoyalResponse } from '../responses';
-import { getBaseAddress } from '../constants';
 
 export interface Fiction {
   type: string;
@@ -28,10 +28,12 @@ export interface FictionStats {
   ratings: number;
   favorites: number;
   followers: number;
+
   views: {
     total: number;
     average: number;
   };
+
   score: {
     style: number;
     story: number;
@@ -46,6 +48,31 @@ export interface FictionAuthor {
   name: string;
   title: string;
   avatar: string;
+}
+
+export interface Review {
+  posted: number;
+  content: string;
+
+  rating: {
+    up: number;
+    down: number;
+  };
+
+  author: {
+    id: number,
+    name: string,
+    avatar: string,
+  };
+
+  score: {
+    overall: number;
+
+    style?: number;
+    story?: number;
+    grammar?: number;
+    character?: number;
+  };
 }
 
 /**
@@ -66,7 +93,7 @@ export class FictionService {
    * @returns - Fiction object.
    */
   public async getFiction(id: number) {
-    const path = `/fiction/${id.toString()}`;
+    const path = `/fiction/${String(id)}`;
     const body = await this.req.get(path);
     const fiction =  FictionParser.parseFiction(body);
 
@@ -85,6 +112,19 @@ export class FictionService {
     const fiction = FictionParser.parseFiction(body);
 
     return new RoyalResponse(fiction);
+  }
+
+  public async getReviews(id: number, page: number | 'last' = 1) {
+    const base = `/fiction/${String(id)}`;
+    const path = base + '?' + new URLSearchParams({
+      page: String(page === 'last'
+        ? FictionParser.getLastPage(await this.req.get(base)) : page),
+    });
+
+    const body = await this.req.get(path);
+    const reviews = FictionParser.parseReviews(body);
+
+    return new RoyalResponse(reviews);
   }
 }
 
@@ -166,5 +206,70 @@ class FictionParser {
 
     return { type, tags, stats, title, image, status,
       author, warnings, chapters, description };
+  }
+
+  public static getLastPage(html: string) {
+    const $ = cheerio.load(html);
+
+    const href = $('ul.pagination').find('li').last()
+      .find('a').attr('href');
+
+    if (!href) {
+      return 1;
+    }
+
+    // Get whatever is before '#' and after '=', (?page=1#comments).
+    const page = parseInt(href.split('#')[0].split('=')[1], 10);
+
+    return page;
+  }
+
+  public static parseReviews(html: string): Review[] {
+    const $ = cheerio.load(html);
+
+    const reviews: Review[] = [];
+
+    $('div.review.row').each((i, el) => {
+      const posted = parseInt($(el).find('time').attr('unixtime'), 10);
+
+      let content = '';
+      $(el).find('div.review-content')
+        .each((j, p) => content += $(p).text() + '\n');
+      content = content.trim();
+
+      const rating = {
+        up: parseInt($(el).find('button.blue-dark').text(), 10),
+        down: parseInt($(el).find('button.red-sunglo').text(), 10),
+      };
+
+      const authorLink = $(el).find('div.review-meta').find('a').eq(0);
+      const author = {
+        name: $(authorLink).text(),
+        avatar: $(el).find('img').attr('href'),
+        id: parseInt($(authorLink).attr('href').split('/')[2], 10),
+      };
+
+      const parseScore = (classAttr: string) => {
+        if (!classAttr) { return -1; }
+
+        const classes = classAttr.split(' ');
+        const value = classes[classes.length - 1].split('-')[1];
+
+        return parseInt(value, 10) / 10;
+      };
+
+      const scoreItems = $(el).find('div.review-side').find('li').find('div');
+      const score = {
+        style: parseScore($(scoreItems).eq(1).attr('class')),
+        story: parseScore($(scoreItems).eq(2).attr('class')),
+        overall: parseScore($(scoreItems).eq(0).attr('class')),
+        grammar: parseScore($(scoreItems).eq(3).attr('class')),
+        character: parseScore($(scoreItems).eq(4).attr('class')),
+      };
+
+      reviews.push({ score, author, rating, content, posted });
+    });
+
+    return reviews;
   }
 }
