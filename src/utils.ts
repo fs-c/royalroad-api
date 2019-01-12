264ba16bb4da3ca917cb1b0bd0cbca0393f2b31e
lw <me@lw.ag>
2019-01-12 21:52:26 +0100

    Add utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+import * as cheerio from 'cheerio';
+
+export const getLastPage = (html: string): number => {
+    const $ = cheerio.load(html);
+
+    const href = $('ul.pagination').find('li').last()
+        .find('a').attr('href');
+
+    if (!href || href === 'javascript:;') {
+        return 1;
+    }
+
+    // Get whatever is before '#' and after '=', (?page=1#comments).
+    return parseInt(href.split('#')[0].split('=')[1], 10);
+};
